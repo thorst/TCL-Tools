3578c4cde05ff138c8a503c4f67e4e857a43af4f
Todd Horst <toddmhorst@yahoo.com>
2015-04-25 19:46:02 -0400

    Create _echo.tcl

---
+++ _echo.tcl
@@ -0,0 +1,20 @@
+if 0 {
+	Purpose:
+		Puts multiple arguments without quotes
+		The simple things in life.
+	
+	Playground:
+		_echo Hey how are [list you doing]
+		>> "Hey how are you doing"
+	
+	Change Log:
+	2015-04-25 Todd Horst
+		Initial version
+	
+}
+#
+
+proc _echo {args} {
+    puts $args
+}
+#
